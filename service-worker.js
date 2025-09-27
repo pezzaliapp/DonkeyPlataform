1fc1298b0d5267c0df3524460b873900ff0af9c9
pezzaliapp <pezzalialessandro@gmail.com>
2025-09-27 18:42:36 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,4 @@
-// Simple cache-first service worker (MIT)
-const CACHE = 'donkey-platform-v1';
+const CACHE = 'donkey-platform-v1-1';
 const ASSETS = [
   './',
   './index.html',
@@ -9,17 +8,8 @@ const ASSETS = [
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
-
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));
 });
-self.addEventListener('activate', (e)=>{
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k===CACHE?null:caches.delete(k))))
-  );
-});
-self.addEventListener('fetch', (e)=>{
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+self.addEventListener('fetch', e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
